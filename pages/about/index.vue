456ee48a26f6645753f474284911ab664595c419
himeno-yusuke <himeno-yusuke@kayac.com>
2018-07-16 13:17:20 +0900

    init and build

---
+++ pages/about/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+    <h1>About</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'About - Netlify Prerendering Demo',
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
---
+++ pages/works/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+    <h1>Works</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Works - Netlify Prerendering Demo',
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
